e46b35db0fa43b2190474dc3b7eb70edfe6251d8
gramatchi <nik98.champion@gmail.com>
2024-08-17 01:06:38 +0200

    step 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,36 @@
-import { useState } from 'react'
-import './App.css'
-import Description from './components/Description/Description'
-import Feedback from './components/Feedback/Feedback'
-import Options from './components/Options/Options'
+import { useState } from "react";
+import "./App.css";
+import Description from "./components/Description/Description";
+import Feedback from "./components/Feedback/Feedback";
+import Options from "./components/Options/Options";
+import Notification from "./components/Notification/Notification";
 
 function App() {
-
   const [feedback, setFeedback] = useState({
     good: 0,
     neutral: 0,
-    bad: 0
+    bad: 0,
   });
 
-  const updateFeedback = feedbackType => {
-    setFeedback(prev => ({
+  const totalFeedback = Object.values(feedback).reduce(
+    (sum, value) => sum + value,
+    0
+  );
+
+  const updateFeedback = (feedbackType) => {
+    setFeedback((prev) => ({
       ...prev,
-      [feedbackType]: prev[feedbackType] + 1
+      [feedbackType]: prev[feedbackType] + 1,
     }));
-  }
+  };
 
   return (
     <>
-      <Description/>
-      <Options options={feedback} handleClick={updateFeedback}/>
-      <Feedback feedback={feedback}/>
+      <Description />
+      <Options options={feedback} handleClick={updateFeedback} />
+      {totalFeedback ? <Feedback feedback={feedback} /> : <Notification />}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
